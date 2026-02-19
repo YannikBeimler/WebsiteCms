d6baed6919838f106fa135e7d52391ca9d1ad85d
YannikBeimler <ybeimler@gmail.com>
2026-02-19 22:50:10 +0100

    ADDED: FirebaseConfig for Emulator

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY || 'demo-api-key',
+  authDomain:
+    process.env.VUE_APP_FIREBASE_AUTH_DOMAIN || 'demo.firebaseapp.com',
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID || 'craniobeimler',
+  storageBucket:
+    process.env.VUE_APP_FIREBASE_STORAGE_BUCKET || 'craniobeimler.appspot.com',
+  messagingSenderId:
+    process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID || '123456789',
+  appId: process.env.VUE_APP_FIREBASE_APP_ID || '1:123456789:web:abcdef123456',
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID || 'G-ABCDEFGHIJ',
 };
 
 const app = initializeApp(firebaseConfig);
